24c7b3ce97ec50334b4ac3d6d764d9a7f09d5030
frostbite69425 <medhihirak3@gmail.com>
2026-05-02 11:17:36 +0530

    add: solution for the factorial problem

--- computer_science/recursion/1_factorial/factorial.js
+++ computer_science/recursion/1_factorial/factorial.js
@@ -1,5 +1,12 @@
-const factorial = function() {
-
+const factorial = function (num) {
+  if (typeof num !== "number" || num < 0 || num % 1 !== 0) {
+    return undefined;
+  }
+  if (num == 0 || num == 1) {
+    return 1;
+  } else {
+    return num * factorial(num - 1);
+  }
 };
 
 // Do not edit below this line
