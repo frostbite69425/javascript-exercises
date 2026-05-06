2acb254b0036dfa3eb8bf76fcad5b9341ba42e0e
frostbite69425 <medhihirak3@gmail.com>
2026-05-06 11:38:22 +0530

    add: solution for the totalIntegers problem

--- computer_science/recursion/3_totalIntegers/totalIntegers.js
+++ computer_science/recursion/3_totalIntegers/totalIntegers.js
@@ -1,6 +1,39 @@
-const totalIntegers = function() {
-  
+const totalIntegers = function (list) {
+  let result = 0;
+
+  if (typeof list !== "object") {
+    return undefined;
+  }
+
+  if (Array.isArray(list)) {
+    for (const sublist of list) {
+      if (
+        sublist !== null &&
+        typeof sublist === "object" &&
+        sublist.length !== 0
+      ) {
+        result = result + totalIntegers(sublist);
+      } else if (typeof sublist === "number" && Number.isInteger(sublist)) {
+        result++;
+      }
+    }
+  } else {
+    const listValues = Object.values(list);
+    for (const sublist of listValues) {
+      if (
+        sublist !== null &&
+        typeof sublist === "object" &&
+        sublist.length !== 0
+      ) {
+        result = result + totalIntegers(sublist);
+      } else if (typeof sublist === "number" && Number.isInteger(sublist)) {
+        result++;
+      }
+    }
+  }
+
+  return result;
 };
-  
+
 // Do not edit below this line
 module.exports = totalIntegers;
