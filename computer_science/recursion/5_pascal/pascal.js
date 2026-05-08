c65e7d78fa290ec00b8472c158feb2279d54c82e
frostbite69425 <medhihirak3@gmail.com>
2026-05-08 18:50:26 +0530

    add: solution for the pascal problem

--- computer_science/recursion/5_pascal/pascal.js
+++ computer_science/recursion/5_pascal/pascal.js
@@ -1,6 +1,18 @@
-const pascal = function() {
-  
+const pascal = function (row) {
+  let result = [];
+  result[0] = 1;
+  result[row - 1] = 1;
+  if (row == 1) {
+    return [1];
+  } else if (row == 2) {
+    return [1, 1];
+  } else {
+    for (let i = 1; i < row - 1; i++) {
+      result[i] = pascal(row - 1)[i - 1] + pascal(row - 1)[i];
+    }
+  }
+  return result;
 };
-  
+
 // Do not edit below this line
 module.exports = pascal;
